63e13a722cabf1e062aca69e7b6896af1d73a13f
jngsoo <wt2933@naver.comf>
2019-06-25 19:03:02 +0900

    rename

---
+++ pyprac.py
@@ -0,0 +1,22 @@
+'''
+입력
+1
+3
+A 10
+B 7
+C 5  
+
+출력
+#1
+AAAAAAAAAA
+BBBBBBBCCC
+CC
+'''
+
+T = int(input())
+for i in range(T):
+    N = int(input())
+
+    for j in range(N):
+        line = input()
+
